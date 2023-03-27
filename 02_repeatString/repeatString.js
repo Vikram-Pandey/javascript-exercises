8808dd3802a8ee1b73609c9d23427773a84847d3
Vikram <pandey.vikram@outlook.com>
2023-03-27 02:31:02 -0500

    02-repeatString completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (val, n) {
+  if (n < 0) {
+    return "ERROR";
+  }
+  let result = "";
+  for (let i = 0; i < n; i++) {
+    result += val;
+  }
 
+  return result;
 };
 
 // Do not edit below this line
