3da537a4f92ecbf766cf3de174c90c62b6180f25
Vikram <pandey.vikram@outlook.com>
2023-04-01 23:31:30 -0500

    05-sumAll Completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (arg1, arg2) {
+  if (Number.isInteger(arg1) == false || Number.isInteger(arg2) == false) {
+    return "ERROR";
+  }
 
+  if (arg1 < 0 || arg2 < 0) {
+    return "ERROR";
+  }
+
+  let large = arg1 > arg2 ? arg1 : arg2;
+  let small = arg1 > arg2 ? arg2 : arg1;
+
+  let sum = 0;
+  for (let i = small; i <= large; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
