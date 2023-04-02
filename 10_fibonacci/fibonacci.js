79e514e5caed7cb35fd18278f63f4de8d3aaf367
Vikram <pandey.vikram@outlook.com>
2023-04-02 04:25:44 -0500

    09 fibonacci completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  let result = 0;
 
+  num = Number(num);
+
+  if (num < 0) {
+    return "OOPS";
+  } else {
+    if (num == 0 || num == 1) {
+      return num;
+    }
+    let num1 = 0;
+    let num2 = 1;
+    for (let i = 2; i <= num; i++) {
+      result = num1 + num2;
+      num1 = num2;
+      num2 = result;
+    }
+  }
+
+  return result;
 };
 
 // Do not edit below this line
