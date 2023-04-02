7a60a415add6af949dcbd485cfed9166377e8782
Vikram <pandey.vikram@outlook.com>
2023-04-01 22:31:12 -0500

    04_removeFromArray Completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+  const resultArray = [];
 
+  return array.filter((item) => !args.includes(item));
 };
 
 // Do not edit below this line
