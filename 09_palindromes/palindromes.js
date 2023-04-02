7ec6f059d9dcabf075acc6b827c985ec6783c717
Vikram <pandey.vikram@outlook.com>
2023-04-02 04:09:22 -0500

    09-palindrome completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  str = str.toLowerCase();
 
+  str = str.replace(/[^a-z]/g, "");
+
+  console.log(str);
+
+  let flag = true;
+
+  for (let i = 0, j = str.length - 1; i < j; i++, j--) {
+    if (str[i] != str[j]) {
+      flag = false;
+    }
+  }
+
+  return flag;
 };
 
 // Do not edit below this line
